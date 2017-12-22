e9878f4f2099b3202b2e8dfb40c77f7ee15af3a4
Miro Svrtan <miro@mirosvrtan.me>
2017-12-22 23:27:35 +0100

    Add ExampleMaker

---
+++ tests/NullDevelopment/Skeleton/ExampleMaker/InstanceExampleTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\NullDevelopment\Skeleton\ExampleMaker;
+
+use NullDevelopment\PhpStructure\DataTypeName\ClassName;
+use NullDevelopment\Skeleton\ExampleMaker\InstanceExample;
+use NullDevelopment\Skeleton\ExampleMaker\SimpleExample;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \NullDevelopment\Skeleton\ExampleMaker\InstanceExample
+ * @group  unit
+ */
+class InstanceExampleTest extends TestCase
+{
+    /** @dataProvider provideSimpleInputs */
+    public function testToString(ClassName $instanceOf, array $parameters, string $expectedStringOutput)
+    {
+        $example = new InstanceExample($instanceOf, $parameters);
+
+        self::assertEquals($expectedStringOutput, $example->__toString());
+    }
+
+    public function provideSimpleInputs(): array
+    {
+        return [
+            [
+                new ClassName('DateTime'),
+                [new SimpleExample('2018-01-01 00:01:00')],
+                "new DateTime('2018-01-01 00:01:00')",
+            ],
+        ];
+    }
+}
