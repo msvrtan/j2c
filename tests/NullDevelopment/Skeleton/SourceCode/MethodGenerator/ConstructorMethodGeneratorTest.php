fa2b1f466af7480fb35d4d0aefb2a298221c82d2
Miro Svrtan <miro@mirosvrtan.me>
2017-12-22 22:36:26 +0100

    Add ConstructorMethodGenerator

---
+++ tests/NullDevelopment/Skeleton/SourceCode/MethodGenerator/ConstructorMethodGeneratorTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\NullDevelopment\Skeleton\SourceCode\MethodGenerator;
+
+use NullDevelopment\PhpStructure\DataType\Property;
+use NullDevelopment\PhpStructure\DataType\Visibility;
+use NullDevelopment\PhpStructure\DataTypeName\ClassName;
+use NullDevelopment\Skeleton\SourceCode\Method\ConstructorMethod;
+use NullDevelopment\Skeleton\SourceCode\MethodGenerator\ConstructorMethodGenerator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \NullDevelopment\Skeleton\SourceCode\MethodGenerator\ConstructorMethodGenerator
+ * @group  unit
+ */
+class ConstructorMethodGeneratorTest extends TestCase
+{
+    /** @var ConstructorMethodGenerator */
+    private $sut;
+
+    public function setUp()
+    {
+        $this->sut = new ConstructorMethodGenerator();
+    }
+
+    /** @dataProvider provideMethods */
+    public function testSupports(ConstructorMethod $method)
+    {
+        self::assertTrue($this->sut->supports($method));
+    }
+
+    /** @dataProvider provideMethods */
+    public function testGenerate(ConstructorMethod $method, string $fileName)
+    {
+        $fileName = __DIR__.'/output/'.$fileName;
+        $expected = @file_get_contents($fileName);
+
+        $result = $this->sut->generate($method);
+
+        if (true === empty($expected)) {
+            file_put_contents($fileName, $result);
+            self::markTestSkipped('Generating output for '.$fileName);
+        } else {
+            self::assertEquals($expected, $result);
+        }
+    }
+
+    public function provideMethods(): array
+    {
+        $firstName = new Property(
+            'firstName',
+            ClassName::create('MyVendor\\User\\UserFirstName'),
+            false,
+            false,
+            false,
+            new Visibility('private')
+        );
+
+        return [
+            [new ConstructorMethod([]), '__construct.empty.output'],
+            [new ConstructorMethod([$firstName]), '__construct.firstName.output'],
+        ];
+    }
+}
