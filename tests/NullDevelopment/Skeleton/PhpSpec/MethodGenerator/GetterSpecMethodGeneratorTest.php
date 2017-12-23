a94b65fc5f9104a72ef13ac0e6e5c4eb23c1c1ac
Miro Svrtan <miro@mirosvrtan.me>
2017-12-23 01:03:00 +0100

    GetterSpecMethod & GetterSpecMethodGenerator

---
+++ tests/NullDevelopment/Skeleton/PhpSpec/MethodGenerator/GetterSpecMethodGeneratorTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\NullDevelopment\Skeleton\PhpSpec\MethodGenerator;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use NullDevelopment\PhpStructure\DataType\Property;
+use NullDevelopment\PhpStructure\DataType\Visibility;
+use NullDevelopment\PhpStructure\DataTypeName\ClassName;
+use NullDevelopment\Skeleton\ExampleMaker\ExampleMaker;
+use NullDevelopment\Skeleton\PhpSpec\Method\GetterSpecMethod;
+use NullDevelopment\Skeleton\PhpSpec\MethodGenerator\GetterSpecMethodGenerator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \NullDevelopment\Skeleton\PhpSpec\MethodGenerator\GetterSpecMethodGenerator
+ * @group  unit
+ */
+class GetterSpecMethodGeneratorTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+    /** @var ExampleMaker */
+    private $exampleMaker;
+    /** @var GetterSpecMethodGenerator */
+    private $sut;
+
+    public function setUp()
+    {
+        $this->exampleMaker = new ExampleMaker();
+        $this->sut          = new GetterSpecMethodGenerator($this->exampleMaker);
+    }
+
+    /** @dataProvider provideMethods */
+    public function testSupports(GetterSpecMethod $method)
+    {
+        self::assertTrue($this->sut->supports($method));
+    }
+
+    /** @dataProvider provideMethods */
+    public function testGenerate(GetterSpecMethod $method, string $fileName)
+    {
+        $fileName = __DIR__.'/output/'.$fileName;
+        $expected = @file_get_contents($fileName);
+
+        $result = $this->sut->generate($method);
+
+        if (true === empty($expected)) {
+            file_put_contents($fileName, $result);
+            self::markTestSkipped('Generating output for '.$fileName);
+        } else {
+            self::assertEquals($expected, $result);
+        }
+    }
+
+    public function provideMethods(): array
+    {
+        $firstName = new Property(
+            'firstName',
+            ClassName::create('MyVendor\\User\\UserFirstName'),
+            false,
+            false,
+            false,
+            new Visibility('private')
+        );
+
+        return [
+            [new GetterSpecMethod('it_exposes_first_name', 'getFirstName', $firstName), 'it_exposes_first_name.output'],
+        ];
+    }
+}
