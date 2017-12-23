516790d89f285cc2e4f96459c6278e631a3b417b
Miro Svrtan <miro@mirosvrtan.me>
2017-12-23 01:47:35 +0100

    Adding Let & Initializable methods & generators

---
+++ src/NullDevelopment/Skeleton/PhpSpec/Method/LetMethod.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NullDevelopment\Skeleton\PhpSpec\Method;
+
+use NullDevelopment\PhpStructure\DataType\Property;
+use Webmozart\Assert\Assert;
+
+/**
+ * @see LetMethodSpec
+ * @see LetMethodTest
+ */
+class LetMethod extends BaseSpecMethod
+{
+    /** @var Property[] */
+    private $properties;
+
+    public function __construct(array $properties)
+    {
+        Assert::allIsInstanceOf($properties, Property::class);
+        $this->properties = $properties;
+    }
+
+    public function getName(): string
+    {
+        return 'let';
+    }
+
+    /** @return Property[] */
+    public function getParameters(): array
+    {
+        return $this->properties;
+    }
+}
