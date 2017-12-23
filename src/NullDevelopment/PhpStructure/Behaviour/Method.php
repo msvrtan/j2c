80bdaaa38ad531737997cdcc4a6baaf31fd39ebf
Miro Svrtan <miro@mirosvrtan.me>
2017-12-23 12:07:25 +0100

    Move NullDevelopment\Skeleton\SourceCode\Method to NullDevelopment\PhpStructure\Behaviour\Method

--- src/NullDevelopment/PhpStructure/Behaviour/Method.php
+++ src/NullDevelopment/PhpStructure/Behaviour/Method.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace NullDevelopment\PhpStructure\Behaviour;
 
+use NullDevelopment\PhpStructure\DataType\Property;
+use NullDevelopment\PhpStructure\DataType\Visibility;
+
 interface Method
 {
+    public function getVisibility(): Visibility;
+
     public function getName(): string;
 
+    /** @return Property[] */
     public function getParameters(): array;
 
-    public function getReturnType(): ?string;
+    public function getReturnType(): string;
+
+    public function isNullableReturnType(): bool;
 }
